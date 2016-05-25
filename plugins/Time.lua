6a1adbbf03410c76d011b72b2fcd473f30e0ce8b
p00ria <amir1300sobatdar@gmail.com>
2016-05-25 21:34:24 +0430

    Create Time.lua

---
+++ plugins/Time.lua
@@ -0,0 +1,14 @@
+function run(msg, matches)
+ local url , res = http.request('http://api.gpmod.ir/time/')
+ if res ~= 200 then
+ return "No connection"
+ end
+ local jdat = json:decode(url)
+ local text = '🕒 ساعت'..jdat.FAtime..' \n'📆 امروز'..jdat.FAdate..'میباشد.\n ----\n’🕒'..jdat.ENtime..'\n📆'..jdat.ENdate.. '\nSignal Bot'
+ return text
+ end
+ return {
+ patterns = {
+  "^[!/#]([Tt][iI][Mm][Ee])$"},
+ run = run
+ }
